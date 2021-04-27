84043ed1eb3f7e2fbf3e473b39e79c9d9ef188f2
NazzyDev <dev2@texxia.com.br>
2021-04-27 17:44:33 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import desenho from './assets/image.svg';
+import logo from './assets/logo.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className="principalmesmo">
+    <p> Use artificial intelligence to your advantage </p>
+    <div className="principal"> 
+      <img id="texxia" src={logo} />
+      <img id="desenho" src={desenho} width={700} />
+      
+      <div className="box">
+        <h1 id="text-login"> Login </h1>
+        <input id="input1" placeholder="Email" type="text"/>
+        <input id="input2" placeholder="Senha" type="text"/>
+        <button type="button" id="buttonlogin">
+          Login
+        </button>
+        <h1 id="text">  Don't have an account?  <span> Register  </span> </h1>
+        <h1 id="text2">  Forgot password </h1>
+      </div>
     </div>
+  </div>
+  
   );
 }
 
